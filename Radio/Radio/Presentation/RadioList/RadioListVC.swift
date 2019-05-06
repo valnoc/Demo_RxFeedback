6cec143f1561fc1cb3d3d64cf7519d9c78db36cf
Valeriy Bezuglyy <valnocorner@gmail.com>
2019-05-06 21:33:26 +0300

    load radio list vc

---
+++ Radio/Radio/Presentation/RadioList/RadioListVC.swift
@@ -0,0 +1,30 @@
+//
+//  RadioListVC.swift
+//  Radio
+//
+//  Created by Valeriy Bezuglyy on 06/05/2019.
+//  Copyright © 2019 Valeriy Bezuglyy. All rights reserved.
+//
+
+import UIKit
+
+public class RadioListVC: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
