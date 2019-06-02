4e4f88f64dcb89dac31490aa0426f469133ab9aa
Valeriy Bezuglyy <valnocorner@gmail.com>
2019-06-02 11:31:23 +0300

    added app coordinator

---
+++ JamMusic/JamMusic/AppCoordinator.swift
@@ -0,0 +1,14 @@
+//
+//  AppCoordinator.swift
+//  JamMusic
+//
+//  Created by Valeriy Bezuglyy on 23/05/2019.
+//  Copyright © 2019 Valeriy Bezuglyy. All rights reserved.
+//
+
+import Foundation
+import Coordinator
+
+class AppCoordinator: BaseAppCoordinator {
+    
+}
